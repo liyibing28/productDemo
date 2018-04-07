ee1d81cbaf5b736bf245055a403bc4746ea63697
liyibing28 <liyibing28@gmail.com>
2018-04-07 16:38:37 +0800

    实现表头浮动

--- resources/views/production/show.blade.php
+++ resources/views/production/show.blade.php
@@ -1,136 +1,178 @@
 @extends('layouts.app')
-
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.css" />
+    <script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js" ></script>
+    <script type="text/javascript" src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js" ></script>
+<style>
+    .fixTable thead {
+        background-color: #fff;
+    }
+</style>
 @section('content')
     <div class="container">
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2">
+                <div class="col-md-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading">未完成订单</div>
+                        <div class="panel-heading">
+                            <p>未完成订单</p>
 
-                        <div class="panel-body">
-                                <table class="table table-bordered">
-                                    <thead>
+                        </div>
+
+                        <div id="goodsList" style="height: 100%; overflow-y: scroll; padding: 0;" class=" col-xs-12">
+                            <table id="textTable"  class="table table-bordered scrollTable">
+                                <thead>
+                                <tr>
+                                    <th>序号</th>
+                                    <th>客编</th>
+                                    <th>下单时间</th>
+                                    <th>计划交期</th>
+                                    <th>规格</th>
+                                    <th>型号</th>
+                                    <th>粗车日期</th>
+                                    <th>粗车人员</th>
+                                    <th>热处理日期</th>
+                                    <th>精车日期</th>
+                                    <th>开刀翼日期</th>
+                                    <th>开刀翼班次</th>
+                                    <th>开刀翼人员</th>
+                                    <th>开顶日期</th>
+                                    <th>开顶班次</th>
+                                    <th>开顶人员</th>
+                                    <th>钻孔日期</th>
+                                    <th>钻孔班次</th>
+                                    <th>钻孔人员</th>
+                                    <th>操作</th>
+                                </tr>
+                                </thead>
+                                <tbody id="testTbody">
+
+                                <?php $num = 0; ?>
+                                @foreach($productions->all() as $production)
+                                    <?php $num++;?>
                                     <tr>
-                                        <th>序号</th>
-                                        <th>客编</th>
-                                        <th>下单时间</th>
-                                        <th>计划交期</th>
-                                        <th>规格</th>
-                                        <th>型号</th>
-                                        <th>粗车日期</th>
-                                        <th>粗车人员</th>
-
-                                        <th>热处理日期</th>
-
-                                        <th>精车日期</th>
-
-                                        <th>开刀翼日期</th>
-                                        <th>开刀翼班次</th>
-                                        <th>开刀翼人员</th>
-
-                                        <th>开顶日期</th>
-                                        <th>开顶班次</th>
-                                        <th>开顶人员</th>
-
-                                        <th>钻孔日期</th>
-                                        <th>钻孔班次</th>
-                                        <th>钻孔人员</th>
-                                        <th>操作</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-
-                                    <?php $num = 0; ?>
-                                    @foreach($productions->all() as $production)
-                                        <?php $num++;?>
-                                        <tr>
-                                            <td>
-                                                {{$production->id}}
-                                            </td>
-                                            <td >
-                                                {{$production->kebian}}
-                                            </td>
-                                            <td>
-                                                {{$production->xiadanshijian}}
-                                            </td>
-                                            <td>
-                                                {{$production->jihuajiaoqi}}
-                                            </td>
-                                            <td>
-                                                {{$production->guige}}
-                                            </td>
-                                            <td>
-                                                {{$production->xinghao}}
-                                            </td>
-                                            <td>
-                                                {{$production->cuche}}
-                                            </td>
-                                            <td>
-                                                {{$production->cuche_type}}
-                                            </td>
-                                            <td>
-                                                {{$production->rechuli}}
-                                            </td>
-                                            <td>
-                                                {{$production->jingche}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaidaoyi}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaidaoyi_banci}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaidaoyi_renyuan}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaiding}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaiding_banci}}
-                                            </td>
-                                            <td>
-                                                {{$production->kaiding_renyuan}}
-                                            </td>
-                                            <td>
-                                                {{$production->zuankong}}
-                                            </td>
-                                            <td>
-                                                {{$production->zuankong_banci}}
-                                            </td>
-                                            <td>
-                                                {{$production->zuankong_renyuan}}
-                                            </td>
-                                            <td>
-                                                <form action="/production/{{$production->id}}/edit" method="get">
+                                        <td>
+                                            {{$production->id}}
+                                        </td>
+                                        <td >
+                                            {{$production->kebian}}
+                                        </td>
+                                        <td>
+                                            {{$production->xiadanshijian}}
+                                        </td>
+                                        <td>
+                                            {{$production->jihuajiaoqi}}
+                                        </td>
+                                        <td>
+                                            {{$production->guige}}
+                                        </td>
+                                        <td>
+                                            {{$production->xinghao}}
+                                        </td>
+                                        <td>
+                                            {{$production->cuche}}
+                                        </td>
+                                        <td>
+                                            {{$production->cuche_type}}
+                                        </td>
+                                        <td>
+                                            {{$production->rechuli}}
+                                        </td>
+                                        <td>
+                                            {{$production->jingche}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaidaoyi}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaidaoyi_banci}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaidaoyi_renyuan}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaiding}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaiding_banci}}
+                                        </td>
+                                        <td>
+                                            {{$production->kaiding_renyuan}}
+                                        </td>
+                                        <td>
+                                            {{$production->zuankong}}
+                                        </td>
+                                        <td>
+                                            {{$production->zuankong_banci}}
+                                        </td>
+                                        <td>
+                                            {{$production->zuankong_renyuan}}
+                                        </td>
+                                        <td>
+                                            <form action="/production/{{$production->id}}/edit" method="get">
                                                 <button class="btn btn-success pull-right" type="submit" >修改订单状态</button>
+                                            </form>
+                                        </td>
+                                        @if(Auth::user()->role == 0)
+                                            <td>
+                                                <form action="/production/{{$production->id}}/delete" method="get">
+                                                    <button class="btn btn-success pull-right" type="submit" >删除订单</button>
                                                 </form>
                                             </td>
-                                            @if(Auth::user()->role == 0)
-                                                <td>
-                                                    <form action="/production/{{$production->id}}/delete" method="get">
-                                                        <button class="btn btn-success pull-right" type="submit" >删除订单</button>
-                                                    </form>
-                                                </td>
-                                            @endif
+                                        @endif
 
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                        </div>
+                                    </tr>
+                                @endforeach
 
-
-
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
     </div>
+    </div>
 
     <script type="text/javascript">
+        $(document).ready(function () {
+
+            var html = '',
+                $ele = $('#testTbody');
+            for(var i = 0; i < 200; i++) {
+                html += "<tr><td>1</td><td>12345</td><td>1234</td><td>123</td><td>12</td></tr>";
+            }
+            $ele.empty().append(html);
+
+            var $fixTable = $('#goodsList .fixTable');
+            $('#goodsList').scroll(function() {
+                var id = '#' + this.id;
+                var scrollTop = $(id).scrollTop() || $(id).get(0).scrollTop,
+                    style = {
+                        'position': 'absolute',
+                        'left': '0',
+                        'right': '0',
+                        'top': scrollTop + 'px'
+                    };
+                if ($fixTable.length) {
+                    (scrollTop === 0) ? $fixTable.addClass('hidden') : $fixTable.removeClass('hidden');
+                    $fixTable.css(style);
+                } else {
+                    var html = $(id + ' .scrollTable thead').get(0).innerHTML;
+                    var table = $('<table class="table table-bordered fixTable"><thead>' + html + '</thead></table>');
+                    table.css(style);
+                    $(id).append(table);
+                    $fixTable = $(this).find('.fixTable');
+                }
+            });
+        })
     </script>
+
 @endsection
+
+
+
+
+
 
